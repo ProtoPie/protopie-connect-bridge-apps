84291839a3a423b482fb0281f670b0caffe55798
Jason <jason@protopie.io>
2022-12-21 18:11:05 +0900

    feat: Add tap and click in node-bridge-robotjs

--- node-bridge-robotjs/messageHandler.js
+++ node-bridge-robotjs/messageHandler.js
@@ -1,4 +1,5 @@
 const robot = require('robotjs');
+robot.setMouseDelay(0);
 
 const oldTouchpadPos = {
   x: 0,
@@ -11,46 +12,64 @@ const oldMousePos = {
 let isMouseDown = false;
 
 const TOUCHPAD_SENSITIVITY = 5;
+const TAP_RANGE = 5;
+
+function checkIsTap(oldTouchpadPosX, oldTouchpadPosY, upPosX, upPosY) {
+  const isXInRange = Math.abs(oldTouchpadPosX - upPosX) <= TAP_RANGE;
+  const isYInRange = Math.abs(oldTouchpadPosY - upPosY) <= TAP_RANGE;
+  const isInRange = isXInRange && isYInRange;
+
+  return isMouseDown && isInRange;
+}
+
+function getXYPosFromString(xyString) {
+  return xyString.split(' ').map((stringifiedNum) => Number(stringifiedNum));
+}
 
 module.exports.messageHandler = (ppMessage) => {
   switch (ppMessage.messageId) {
-    case 'down_x':
-      oldTouchpadPos.x = Number(ppMessage.value);
-      oldMousePos.x = robot.getMousePos().x;
-      isMouseDown = true;
-      return;
-    case 'down_y':
-      oldTouchpadPos.y = Number(ppMessage.value);
-      oldMousePos.y = robot.getMousePos().y;
-      isMouseDown = true;
-      return;
-    case 'up_x':
-      isMouseDown = false;
-      return;
-    case 'up_y':
-      isMouseDown = false;
-      return;
-    default:
-      // mouse_x, mouse_y
+    case 'down_pos': {
+      const [x, y] = getXYPosFromString(ppMessage.value);
 
+      oldTouchpadPos.x = x;
+      oldTouchpadPos.y = y;
+
+      const { x: mousePosX, y: mousePosY } = robot.getMousePos();
+      oldMousePos.x = mousePosX;
+      oldMousePos.y = mousePosY;
+
+      isMouseDown = true;
+
+      break;
+    }
+
+    case 'up_pos': {
+      const [x, y] = getXYPosFromString(ppMessage.value);
+      const isTap = checkIsTap(oldTouchpadPos.x, oldTouchpadPos.y, x, y);
+
+      if (isTap) {
+        robot.mouseClick();
+      }
+
+      isMouseDown = false;
+      break;
+    }
+
+    case 'move_pos': {
       if (!isMouseDown) {
         return;
       }
 
-      robot.setMouseDelay(1);
-      if (ppMessage.messageId === 'mouse_x') {
-        const newTouchpadX = Number(ppMessage.value);
-        robot.moveMouse(
-          oldMousePos.x + (newTouchpadX - oldTouchpadPos.x) * TOUCHPAD_SENSITIVITY,
-          robot.getMousePos().y
-        );
-      } else {
-        const newTouchpadY = Number(ppMessage.value);
-        robot.moveMouse(
-          robot.getMousePos().x,
-          oldMousePos.y + (newTouchpadY - oldTouchpadPos.y) * TOUCHPAD_SENSITIVITY
-        );
-      }
+      const [x, y] = getXYPosFromString(ppMessage.value);
+
+      robot.moveMouse(
+        oldMousePos.x + (x - oldTouchpadPos.x) * TOUCHPAD_SENSITIVITY,
+        oldMousePos.y + (y - oldTouchpadPos.y) * TOUCHPAD_SENSITIVITY
+      );
+      break;
+    }
+
+    default:
       break;
   }
 };
